b31cd3d91e60c27842689feea5436d298079fa25
Reaper <ahoy@barelyhuman.dev>
2022-06-16 11:38:03 +0530

    hardcode height and width to fix icons for safari

--- islands/Player.tsx
+++ islands/Player.tsx
@@ -6,6 +6,12 @@ import { shortcuts } from "../utils/css.ts";
 interface PlayerProps {}
 
 export default function Player(props: PlayerProps) {
+  const [playing, setPlaying] = useState(false);
+
+  const onToggle = ()=>{
+    setPlaying(!playing)
+  }
+
   return (
     <div
       class={tw`flex w-full flex-wrap items-center rounded-md bg-zinc-900 p-4 md:flex-nowrap`}
@@ -18,23 +24,39 @@ export default function Player(props: PlayerProps) {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
+            class="h-10 w-10"
             viewBox="0 0 256 256"
           >
             <rect width="256" height="256" fill="none"></rect>
             <path d="M56,32a8,8,0,0,1,8,8v73.7L183.7,40.6A16,16,0,0,1,208,54.3V201.7a15.9,15.9,0,0,1-8.2,14,15.4,15.4,0,0,1-7.8,2,16.2,16.2,0,0,1-8.3-2.3L64,142.3V216a8,8,0,0,1-16,0V40A8,8,0,0,1,56,32Z"></path>
           </svg>
         </button>
-        <button
-          class={tw`${shortcuts.buttons} h-10 w-10 rounded-full bg-accent-500 p-3 hover:bg-opacity-75`}
+        <button 
+        onClick={onToggle}
+          class={tw`${shortcuts.buttons} bg-accent-500 h-10 w-10 rounded-full p-3 hover:bg-opacity-75`}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 256 256"
-          >
-            <rect width="256" height="256" fill="none"></rect>
-            <path d="M232.3,114.3,88.3,26.4a15.5,15.5,0,0,0-16.1-.3A15.8,15.8,0,0,0,64,40V216a15.8,15.8,0,0,0,8.2,13.9,15.5,15.5,0,0,0,16.1-.3l144-87.9a16,16,0,0,0,0-27.4Z"></path>
-          </svg>
+          {!playing && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              class="h-10 w-10"
+              viewBox="0 0 256 256"
+            >
+              <rect width="256" height="256" fill="none"></rect>
+              <path d="M232.3,114.3,88.3,26.4a15.5,15.5,0,0,0-16.1-.3A15.8,15.8,0,0,0,64,40V216a15.8,15.8,0,0,0,8.2,13.9,15.5,15.5,0,0,0,16.1-.3l144-87.9a16,16,0,0,0,0-27.4Z"></path>
+            </svg>
+          )}
+          {playing && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              class="h-10 w-10"
+              viewBox="0 0 256 256"
+            >
+              <rect width="256" height="256" fill="none"></rect>
+              <path d="M216,48V208a16,16,0,0,1-16,16H164a16,16,0,0,1-16-16V48a16,16,0,0,1,16-16h36A16,16,0,0,1,216,48ZM92,32H56A16,16,0,0,0,40,48V208a16,16,0,0,0,16,16H92a16,16,0,0,0,16-16V48A16,16,0,0,0,92,32Z"></path>
+            </svg>
+          )}
         </button>
         <button
           class={tw`${shortcuts.buttons} h-10 w-10 rounded-full bg-zinc-800 p-3 hover:bg-opacity-75`}
@@ -42,6 +64,7 @@ export default function Player(props: PlayerProps) {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
+            class="h-10 w-10"
             viewBox="0 0 256 256"
           >
             <rect width="256" height="256" fill="none"></rect>
